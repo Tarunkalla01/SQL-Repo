0a4399ef87f457cc71b9978644be0765c0e1516f
Tarun kalla <tarunkalla0125@gmail.com>
2025-12-16 13:26:42 +0530

    Add files via upload

--- Sql_practice.sql
+++ Sql_practice.sql
@@ -1,4 +1,6 @@
 SELECT * FROM world.country;
+--  Q)  you need to get govtForm and the total number of country for each govtform where the total countries is greater than 20
 select GovernmentForm , count(name) from country group by GovernmentForm having count(name)>20;
+
 select GovernmentForm , count(name) from country where Capital>30 group by GovernmentForm;
 select count(name) , count(Region) ,sum(Population) from country where LifeExpectancy>38 group by continent having sum(Population)>300000;
